647c47d0278ee3610af47e51e532d919154ae84d
Sreedhar Joshi <joshi.sreedhar@gmail.com>
2018-03-19 15:58:04 +0530

    Implement basic player toggle functianility

--- src/reducers.test.js
+++ src/reducers.test.js
@@ -2,11 +2,11 @@ import rootReducer from './reducers';
 import {handlePlayerMove, PLAYER_PLAYED} from './actions';
 
 it("should change active player when CHANGE_PLAYER is dispatched", ()=>{
-    const state = {};
-    const action ={};
+    const currentState = {players:[{symbol: "O", id: "p1"}, {symbol: "X", id: "p2"}],activePlayer:{symbol: "O", id: "p1"}};
+    const action =handlePlayerMove({});
 
-    const newState = handlePlayerMove(state, action);
-    const expectedState = {};
+    const newState = rootReducer(currentState, action);
+    const expectedState = {players:[{symbol: "O", id: "p1"}, {symbol: "X", id: "p2"}],activePlayer:{symbol: "X", id: "p2"}};
 
-    expect(JSON.stringify(newState.activeplayer)).toEqual(JSON.stringify());
+    expect(JSON.stringify(newState)).toEqual(JSON.stringify(expectedState));
 });
