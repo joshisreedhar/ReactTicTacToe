68494198b60fa29f43e7f89da172bc26fb11fa38
Sreedhar Joshi <joshi.sreedhar@gmail.com>
2018-03-20 20:59:20 +0530

    Associate selected cells with players

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,15 @@
 import {PLAYER_PLAYED} from './actions';
 import {togglePlayer} from './helper';
+import {get} from 'lodash';
 
 const rootReducer = function(state,action){
     switch(action.type){
         case PLAYER_PLAYED:
             const nextActivePlayer = togglePlayer(state);
+            let currentPlayer = state.players.find(function(player){return player.id === state.activePlayer.id});
+            let cells = get(currentPlayer, 'cells',[]);
+            cells.push(action.cellIndex);
+            currentPlayer.cells = cells;
             return Object.assign({},state,{activePlayer:nextActivePlayer});
         default:
             return state;
--- src/reducers.test.js
+++ src/reducers.test.js
@@ -1,12 +1,12 @@
 import rootReducer from './reducers';
 import {handlePlayerMove, PLAYER_PLAYED} from './actions';
 
-it("should change active player when CHANGE_PLAYER is dispatched", ()=>{
+it("should change active player and set player cell when CHANGE_PLAYER is dispatched", ()=>{
     const currentState = {players:[{symbol: "O", id: "p1"}, {symbol: "X", id: "p2"}],activePlayer:{symbol: "O", id: "p1"}};
-    const action =handlePlayerMove({});
+    const action =handlePlayerMove({row:"1", cell:"1"});
 
     const newState = rootReducer(currentState, action);
-    const expectedState = {players:[{symbol: "O", id: "p1"}, {symbol: "X", id: "p2"}],activePlayer:{symbol: "X", id: "p2"}};
+    const expectedState = {players:[{symbol: "O", id: "p1", cells:[{row:"1", cell:"1"}]}, {symbol: "X", id: "p2"}], activePlayer:{symbol: "X", id: "p2"}};
 
     expect(JSON.stringify(newState)).toEqual(JSON.stringify(expectedState));
 });
